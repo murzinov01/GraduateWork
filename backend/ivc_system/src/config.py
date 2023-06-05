459f734c9a41ff703b38f41fbbb60aca580e9f6c
Murzinov Michail <murzinov01@bk.ru>
2023-06-06 00:02:51 +0300

    hot fix 3

--- backend/ivc_system/src/config.py
+++ backend/ivc_system/src/config.py
@@ -2,9 +2,7 @@ import os
 
 
 # DATABASE
-# SQLALCHEMY_DATABASE_URL = os.getenv("SQLALCHEMY_DATABASE_URL", "postgresql://postgres:postgrespw@localhost:5432")
-SQLALCHEMY_DATABASE_URL = os.getenv("SQLALCHEMY_DATABASE_URL", "")
-print(f"{SQLALCHEMY_DATABASE_URL=}")
+SQLALCHEMY_DATABASE_URL = os.getenv("SQLALCHEMY_DATABASE_URL", "postgresql://postgres:postgrespw@localhost:5432")
 
 # JWT Generation
 SECRET_KEY = "01d25e094faa6ca3556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
